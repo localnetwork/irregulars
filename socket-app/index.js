1b9db9dce9c70cd196adb085ad27d500ab988812
Dion Halcyon <diomenikep@gmail.com>
2024-03-23 14:33:53 +0800

    chore: add sounds

--- socket-app/index.js
+++ socket-app/index.js
@@ -2,8 +2,15 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 
+const path = require("path");
+
 const app = express();
 const server = http.createServer(app);
+
+const assetsDirectory = path.join(__dirname, "assets");
+
+// Serve static files from the assets directory
+app.use("/assets", express.static(assetsDirectory));
 
 const io = socketIo(server);
 
